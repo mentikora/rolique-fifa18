40b5315dfca1d3e2c08260e519d9ec9572525c5b
Mentikora <notender_boy@ukr.net>
2018-06-14 03:21:32 +0300

    upd

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -3,6 +3,89 @@ import './styles.css';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const playersData = [
+  {
+    name: 'Anton',
+    surname: 'Yatseniuk',
+    nickname: 'Otec',
+    image: '/img/anton.png',
+    position: 'FEdev',
+    role: 'Cristiano Ronaldo',
+    align: 'left',
+    pros: 'dribling, situation on the fiplayerd'
+  },
+  {
+    name: 'Volodymyr',
+    surname: 'Paskiv',
+    nickname: 'YaPridymav',
+    image: '/img/vova.png',
+    position: 'BEdev',
+    role: 'Lionel Messi',
+    align: 'right',
+    pros: 'dribling (no), pass'
+  },
+  {
+    name: 'Roman',
+    surname: 'Malec',
+    nickname: 'RapGod',
+    image: '/img/malec.png',
+    position: 'BEdev',
+    role: 'Neymar da Silva Santos Júnior',
+    align: 'left',
+    pros: 'game knowledge, free kick'
+  },
+  {
+    name: 'Igor',
+    surname: 'Semenyuk',
+    nickname: 'Schleunigalover',
+    image: '/img/igor.png',
+    position: 'BEdev',
+    role: 'Paulo Dybala',
+    align: 'right',
+    pros: 'Italy teams, corners'
+  },
+  {
+    name: 'Vladyslav',
+    surname: 'Bilyi',
+    nickname: 'PHP>all',
+    image: '/img/vlad.png',
+    position: 'BEdev',
+    role: 'Olivier Giroud',
+    align: 'left',
+    pros: 'runs, pass'
+  },
+  {
+    name: 'Markiian',
+    surname: 'Linynskyi',
+    nickname: 'borschtsch',
+    image: '/img/markiyan.png',
+    position: 'FEdev',
+    role: 'Leroy Sane',
+    align: 'right',
+    pros: 'long pass, flank'
+  },
+  {
+    name: 'Nazar',
+    surname: 'Vasylyshyn',
+    nickname: 'break the walls',
+    image: '/img/nazar.png',
+    position: 'design',
+    role: 'Harry Kane',
+    align: 'left',
+    pros: 'positioning, dribling'
+  },
+  {
+    name: 'Roman',
+    surname: 'Horobets',
+    nickname: 'css4food',
+    image: '/img/horo.png',
+    position: 'FEdev',
+    role: 'Andriy Pyatov',
+    align: 'right',
+    pros: 'defence, flank'
+  }
+];
+
 const Tabs = () => (
   <Router>
     <div className="tabs container">
@@ -15,9 +98,6 @@ const Tabs = () => (
             <Link to="/bracket">Bracket</Link>
           </li>
           <li>
-            <Link to="/participants">Participants</Link>
-          </li>
-          <li>
             <Link to="/rules">Rules</Link>
           </li>
         </ul>
@@ -25,7 +105,6 @@ const Tabs = () => (
 
       <Route exact path="/" component={ Overview } />
       <Route path="/bracket" component={ Bracket } />
-      <Route path="/participants" component={ Participants } />
       <Route path="/rules" component={ Rules } />
     </div>
   </Router>
@@ -33,8 +112,35 @@ const Tabs = () => (
 
 const Overview = () => (
   <div>
-    <div className="block">
-      Overview
+    <div className="players_wrapper">
+      {
+        playersData.map( (player, key) => (
+          <div className={`player player-${player.align} player-${key}`} key={key}>
+            <div className="player__image">
+              <img src={window.location.origin + player.image}/>
+            </div>
+            <div className="player__content">
+              <span>
+                {player.role}
+                <span className="divider">/</span>
+                {player.position}
+              </span>
+              <h2 className="player__name">
+                {player.name}
+                <span className="player__nickname">
+                  `{player.nickname}`
+                </span>
+                {player.surname}
+              </h2>
+              <div>
+                <div className="player__pros">
+                  Cons: {player.pros}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      }
     </div>
   </div>
 );
@@ -43,14 +149,6 @@ const Bracket = () => (
   <div>
     <div className="block">
       Bracket
-    </div>
-  </div>
-);
-
-const Participants = () => (
-  <div>
-    <div className="block">
-      Participants
     </div>
   </div>
 );
@@ -74,7 +172,7 @@ const Rules = () => (
               In all situations possible, tournament referees will monitor the game situation so that it may be restored in the event of game interruptions.
             </li>
             <li>
-              If a Player has an issue that they feel is creating an unfair advantage for the opponent, they must pause the game, (or request their opponent pause the game) and bring the issue to the attention of the referee. The referee will address the issue. Referee - rolique team.
+              If a Player has an issue that they feplayer is creating an unfair advantage for the opponent, they must pause the game, (or request their opponent pause the game) and bring the issue to the attention of the referee. The referee will address the issue. Referee - rolique team.
             </li>
           </ul>
         </li>
@@ -87,7 +185,7 @@ const Rules = () => (
               Each Team can pause the game up to five times. If a Team pauses the game, it must be to make a tactical change to their squad, which can include substitutions, formation changes, or tactic adjustments.
             </li>
             <li>
-              Team are only allowed to pause the game when the ball is not in play, meaning it has left the field of play or a foul has been committed and it’s a dead ball situation, or if the Team holds possession of their ball in their half of the field.
+              Team are only allowed to pause the game when the ball is not in play, meaning it has left the fiplayerd of play or a foul has been committed and it’s a dead ball situation, or if the Team holds possession of their ball in their half of the fiplayerd.
             </li>
           </ul>
         </li>
