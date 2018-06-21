ed00530a6552f97041eccafc46f6b200970073a9
Roman Horobets <roman.horobets@rolique.io>
2018-06-21 12:27:57 +0300

    htaccess, tinyimages, webfont components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 
+import WebFont from 'webfontloader';
+
+
 import Info from './components/info';
 import Tabs from './components/tabs';
 import TabContent from './components/tab-content';
 
 import './styles.css';
+
+WebFont.load({
+  google: {
+    families: ['Encode+Sans+Condensed:300,400,600']
+  }
+});
 
 class App extends Component {
   render() {
