75fc3dca174ca144bd14cb1f998cb553b864e90b
Roman Horobets <roman.horobets@rolique.io>
2018-07-02 12:27:58 +0300

    close #19

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -19,18 +19,6 @@ class MemoriesSlider extends Component {
         <div>
           <h3>2</h3>
         </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
       </Slider>
     )
   }
