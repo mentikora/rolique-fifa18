5931b4c6869d07b3ebbc35893ed1aded7a1c816c
mentikora <notender_boy@ukr.net>
2019-02-04 11:12:50 +0200

    parallax, parallax item, font, logo

---
+++ src/components/logo/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './styles.css'
+export default () => (
+  <div className="logo">
+    Rolique's
+    <br/>
+    Soccer
+  </div>
+)
--- src/containers/parallax/parallax.js
+++ src/containers/parallax/parallax.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ParallaxItem from './parallaxItem'
 import './styles.css'
+import Logo from '../../components/logo/index';
 
 import img1 from './images/bg1.png'
 import img2 from './images/bg2.png'
 import img3 from './images/bg3.png'
+import img4 from './images/bg4.png'
+import img5 from './images/bg5.png'
 
 const items = [
-  img1, img2, img3
+  img1, img2, img3, img4, img5
 ]
 
 class Parallax extends React.Component {
@@ -35,18 +38,21 @@ class Parallax extends React.Component {
   render() {
 
     return(
-      <div className="parallax" onScroll={this.handleScroll}>
-        {
-          items.map( (item, value) => {
-            return <ParallaxItem
-              key={value}
-              backgroundImage={item}
-              transformStep={value + 1 * 0.5}
-              transformY={this.state.scroll / 10}
-            />
-          })
-        }
-      </div>
+      <React.Fragment>
+        <Logo />
+        <div className="parallax" onScroll={this.handleScroll}>
+          {
+            items.map( (item, value) => {
+              return <ParallaxItem
+                key={value}
+                backgroundImage={item}
+                transformStep={value + 1 * 0.5}
+                transformY={this.state.scroll / 10}
+              />
+            })
+          }
+        </div>
+      </React.Fragment>
     )
   }
 }
