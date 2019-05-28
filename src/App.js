c24f961ecbfe75f2fe3d7d815170ecbaa48f456a
mentikora <roman.horobets@rolique.io>
2019-05-28 14:51:53 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react'
 import './styles.css'
-
-import RSlider from './containers/slider'
-import Parallax from './containers/parallax/parallax'
-import About from './containers/about/index'
+import Ze from './components/ze';
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <div className='page-layout'>
-          <Parallax />
-          <About />
-          <RSlider />
-        </div>
+        <Ze />
       </div>
     )
   }
---
+++ src/components/ze/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+const Ze = () => {
+  return(
+    <div className="ze">
+      <h1 className="le">заберемо це разом</h1>
+      <p>
+        <span className="ns">зе!</span>
+        <span className="ky">паша - володар кубка</span>
+      </p>
+      <p className="perdisent">
+        #roliquefifaspring
+      </p>
+    </div>
+  )
+}
+
+export default Ze;
